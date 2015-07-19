cfcf1b461c35b30a741f495a50478bdda836507d
Arjan Frans <arjanfrans.com@gmail.com>
2015-07-19 14:07:15 +0200

    removed slow theme scripts

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -9,9 +9,33 @@ dotinstall() {
     fi
 }
 
-settheme() {
+gtheme() {
 	source "$HOME/.dotfiles/colorschemes/base16-builder/output/gnome-terminal/$1.sh"
 }
+
+# Change the shell theme along with vim colorscheme
+stheme() {
+	if [ $1 == "list" ]; then
+		ls $HOME/.dotfiles/colorschemes/base16-builder/output/shell;
+		return
+	fi
+	script="$HOME/.dotfiles/colorschemes/base16-builder/output/shell/$1.sh"
+	script_name=$(basename $script .sh)
+	theme=${script_name#*-}
+	variation=${theme#*.}
+	theme=${theme%.*}
+
+	source $script
+	ln -fs $script ~/.base16_theme
+	export BASE16_THEME=base16-{$theme}
+	export BASE16_VARIATION=$variation
+	if type tmux_${variation} >/dev/null; then
+		tmux_${variation}
+	fi
+	[ -f ~/.vimrc_background ] && rm ~/.vimrc_background
+	echo -e "set background=$variation\ncolorscheme base16-$theme\n" > ~/.vimrc_background
+}
+
 
 ramdisk() {
     sudo mkdir -p /mnt/ramdisk
