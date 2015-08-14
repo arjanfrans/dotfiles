0b3293d0d08c578108939839bd1a5efe12f27a98
Arjan Frans <arjanfrans.com@gmail.com>
2015-08-14 20:30:10 +0200

    Added indicators script

---
+++ scripts/indicators.sh
@@ -0,0 +1,8 @@
+sudo add-apt-repository ppa:caffeine-developers/ppa
+sudo add-apt-repository ppa:indicator-brightness/ppa
+
+sudo apt-get update
+
+sudo apt-get install indicator-brightness
+sudo apt-get install caffeine
+
