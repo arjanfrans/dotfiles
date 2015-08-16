92de1f98fa9d2ff0bc2fbe4d59fce66fa4b3de80
Arjan Frans <arjanfrans.com@gmail.com>
2015-08-16 21:24:24 +0200

    Added sound indicator

--- scripts/indicators.sh
+++ scripts/indicators.sh
@@ -1,8 +1,11 @@
-sudo add-apt-repository ppa:caffeine-developers/ppa
 sudo add-apt-repository ppa:indicator-brightness/ppa
-
+sudo add-apt-repository ppa:caffeine-developers/caffeine-dev
+udo add-apt-repository ppa:indicator-multiload/stable-daily
+sudo apt-add-repository ppa:yktooo/ppa
 sudo apt-get update
 
 sudo apt-get install indicator-brightness
 sudo apt-get install caffeine
-
+sudo apt-get install indicator-cpufreq
+sudo apt-get install indicator-multiload
+sudo apt-get install indicator-sound-switcher
