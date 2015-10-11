0d00fbdb24ffb7b9a1ac58611fbf987aefaee465
Arjan Frans <arjanfrans.com@gmail.com>
2015-10-11 13:36:54 +0200

    Added indicators script

--- scripts/indicators.sh
+++ scripts/indicators.sh
@@ -1,11 +1,11 @@
-sudo add-apt-repository ppa:indicator-brightness/ppa
-sudo add-apt-repository ppa:caffeine-developers/caffeine-dev
-udo add-apt-repository ppa:indicator-multiload/stable-daily
-sudo apt-add-repository ppa:yktooo/ppa
+sudo add-apt-repository -y ppa:indicator-brightness/ppa
+sudo add-apt-repository -y ppa:caffeine-developers/caffeine-dev
+udo add-apt-repository -y ppa:indicator-multiload/stable-daily
+sudo apt-add-repository -y ppa:yktooo/ppa
 sudo apt-get update
 
-sudo apt-get install indicator-brightness
-sudo apt-get install caffeine
-sudo apt-get install indicator-cpufreq
-sudo apt-get install indicator-multiload
-sudo apt-get install indicator-sound-switcher
+sudo apt-get install -y indicator-brightness
+sudo apt-get install -y caffeine
+sudo apt-get install -y indicator-cpufreq
+sudo apt-get install -y indicator-multiload
+sudo apt-get install -y indicator-sound-switcher
