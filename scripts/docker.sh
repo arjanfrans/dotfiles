6dbe812f828b31a43794ec6188873f5aabf6a450
Arjan Frans <arjanfrans.com@gmail.com>
2017-01-18 18:56:01 +0100

    Add docker compose

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -10,3 +10,8 @@ sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
 sudo apt-get update
 sudo apt-get install docker-engine
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
