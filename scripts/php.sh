0810fd9da00514764fc8fc9d72f2ab3c7e99a91a
Arjan Frans <arjanfrans.com@gmail.com>
2017-01-18 18:42:05 +0100

    Add php,mysql,docker

--- scripts/php.sh
+++ scripts/php.sh
@@ -1,6 +1,7 @@
 sudo apt-get install php7.0 php7.0-fpm php7.0-mysql -y
 sudo apt-get install php7.0-xml
 sudo apt-get install php7.0-intl
+sudo apt-get install php7.0-mbstring
 
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
