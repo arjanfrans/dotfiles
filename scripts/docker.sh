a150fecb34bd91dd74bca3f0c909a55c8a38f2bb
Arjan Frans <arjan.frans@fusonic.net>
2022-11-29 14:07:07 +0100

    add shutter screenshot tool

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,7 +2,7 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
--- scripts/python.sh
+++ scripts/python.sh
@@ -1,2 +1,2 @@
-sudo apt install python-pip
-sudo apt install python3-pip
+sudo apt install python-pip -y
+sudo apt install python3-pip -y
